6e075590a7feb2260a2a4c3af88d48ddfbd59b47
zzz <921968706@qq.com>
2024-02-28 17:27:34 +0800

    main

--- src/sub.py
+++ src/sub.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data_1 = pd.read_csv('HMS-HBA-Baseline/data/submission_35.csv') #0.35
 data_2 = pd.read_csv('HMS-HBA-Baseline/data/submission_40.csv')
@@ -12,6 +13,8 @@ data_dict_2 = row_data_2.to_dict()
 
 excluded_label = 'eeg_id'
 
+labels = [label for label in data_dict_1.keys() if label != excluded_label]
+
 values_1 = [value for label, value in data_dict_1.items() if label != excluded_label]
 
 print(values_1)
@@ -20,11 +23,21 @@ values_2 = [value for label, value in data_dict_2.items() if label != excluded_l
 
 print(values_2)
 
-plt.hist(values_1, bins=10, edgecolor='black', color='blue', alpha=0.5, label='Data 1')  # alpha 参数设置透明度
-plt.hist(values_2, bins=10, edgecolor='black', color='red', alpha=0.5, label='Data 2')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.title('Histogram')
-plt.legend()  # 显示图例
-plt.grid(True)  # 添加网格线
+# 设置每个柱形的宽度
+bar_width = 0.35
+
+# 创建 x 轴位置
+x = np.arange(len(labels))
+
+# 绘制直方图
+plt.bar(x - bar_width/2, values_1, width=bar_width, color='blue', label='0.35')
+plt.bar(x + bar_width/2, values_2, width=bar_width, color='red', label='0.4')
+
+# 设置 x 轴标签和标题
+plt.xlabel('Label')
+plt.ylabel('Value')
+plt.title('Histogram by Label')
+plt.xticks(x + bar_width / 2, labels)
+plt.legend()
+
 plt.show()
